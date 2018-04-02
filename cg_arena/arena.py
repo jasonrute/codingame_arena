ace0578b331cec8f9ef295581ef51aa1ae254847
Jason Rute <jason.rute@gmail.com>
2018-04-02 16:26:33 -0400

    Clean up code

--- cg_arena/arena.py
+++ cg_arena/arena.py
@@ -51,13 +51,13 @@ class Arena:
         """
 
         # parameters (set to defaults)
-        self.number_of_games = 10 # Defaults to 10
+        self.number_of_games = 10  # Defaults to 10
         self.time_limits = False
         self.verbose = False
-        self.show_map = False # Only works if verbose and show_map are both True
-        self.game_arity = None # If set to 2, 3, or 4 it only plays 2, 3, 4 player games
-        self.play_single_game = False
-        self.play_double_game = False
+        self.show_map = False  # Only works if verbose and show_map are both True
+        self.game_arity = None  # If set to 2, 3, or 4 it only plays 2, 3, 4 player games
+        self.single_game = False
+        self.double_game = False
         self.config_str = None
         self.player_list = []
 
@@ -78,37 +78,37 @@ class Arena:
             sys.exit(2)
 
         for opt, arg in opts:
-            if opt == '-t': # Turn on time limits
+            if opt == '-t':  # Turn on time limits
                 self.time_limits = True
-            elif opt == '-v': # Show details of each turn
+            elif opt == '-v':  # Show details of each turn
                 self.verbose = True
-            elif opt == '-m': # Show game map (assuming -v or -s is set)
+            elif opt == '-m':  # Show game map (assuming -v or -s is set)
                 self.show_map = True
-            elif opt in ('-2', '-3', '-4'): # Play <2,3,4> player games only
+            elif opt in ('-2', '-3', '-4'):  # Play <2,3,4> player games only
                 n = int(opt[1])
                 if len(args) > n:
                     print("Too many bots for a", n, "player game.")
                     sys.exit(2)
                 self.game_arity = n
-            elif opt == '-n': # Set number of games
+            elif opt == '-n':  # Set number of games
                 self.number_of_games = int(arg)
-            elif opt == '-s': # Play a single game with a particular given .
+            elif opt == '-s': # Play a single game with a particular given
                               # configuration.  Some other arguments are ignored.
-                if self.play_double_game:
+                if self.double_game:
                     print("Can't play a single and a double game.")
                     sys.exit(2)
-                self.play_single_game = True
+                self.single_game = True
                 self.config_str = arg
-            elif opt == '-d': # Play a double game with a particular given .
-                              # configuration.  It should be called instead of
-                              # -s.
-                if self.play_single_game:
+            elif opt == '-d':  # Play a double game with a particular given .
+                               # configuration.  It should be called instead of
+                               # -s.
+                if self.single_game:
                     print("Can't play a single and a double game.")
                     sys.exit(2)
                 elif len(set(args)) != 2:
                     print("Double game only works for 2 bots right now.")
                     sys.exit(2)
-                self.play_double_game = True
+                self.double_game = True
                 self.config_str = arg
 
         self.player_list = args
@@ -116,125 +116,132 @@ class Arena:
     def run(self):
         """Run the application according to the provided arguments."""
 
-        if self.play_single_game or self.play_double_game:
+        if self.single_game or self.double_game:
             self.verbose = True
 
-            if self.play_single_game:
-                try:
-                    # Initiallization
-                    match = Match(0, self.config_str, self.player_list, self.time_limits, self.verbose, self.show_map)
+            if self.single_game:
+                self.play_single_game()
 
-                    match.pregame()
-                    # Game Loop
-                    while match.is_active():
-                        # Play one round of the game
-                        match.one_turn()
-
-                    # Handle end of game details
-                    match.end_of_game()
-
-                except:
-                    raise
-                finally:
-                    # Kill all subprocesses even if a crash
-                    for p in match.player_processes:
-                        if p: p.kill()
-            else: # double_game (show side by side)
-                player_names = list(set(self.player_list))
-                reverse_player_list = []
-                for p in self.player_list:
-                    reverse_player_list.append(player_names[1] if p==player_names[0]
-                                               else player_names[0])
-                try:
-                    # Initiallization (don't pass show map)
-                    match0 = Match(0, self.config_str, self.player_list, self.time_limits, self.verbose, False)
-                    match1 = Match(1, self.config_str, reverse_player_list, self.time_limits, self.verbose, False)
-
-                    stdout_ = sys.stdout #Keep track of the previous value.
-
-                    stream0 = io.StringIO()
-                    sys.stdout = stream0 # capture stdout to a stream
-                    match0.pregame()
-
-                    stream1 = io.StringIO()
-                    sys.stdout = stream1 # capture stdout to a stream
-                    match1.pregame()
-
-                    sys.stdout = stdout_ # restore the previous stdout.
-                    print_side_by_side(stream0.getvalue(), stream1.getvalue())
-
-                    stream0.close()
-                    stream1.close()
-
-                    # Game Loop
-                    while match0.is_active() or match1.is_active():
-                        # Play one round of each game
-                        stream0 = io.StringIO()
-                        sys.stdout = stream0 # capture stdout to a stream
-                        if match0.is_active():
-                            match0.one_turn()
-
-                        stream1 = io.StringIO()
-                        sys.stdout = stream1 # capture stdout to a stream
-                        if match1.is_active():
-                            match1.one_turn()
-
-                        sys.stdout = stdout_ # restore the previous stdout.
-                        print_side_by_side(stream0.getvalue(), stream1.getvalue())
-
-                        stream0.close()
-                        stream1.close()
-
-                        if self.show_map:
-                            stream0 = io.StringIO()
-                            sys.stdout = stream0 # capture stdout to a stream
-                            match0.print_board() # not implemented yet
-
-                            stream1 = io.StringIO()
-                            sys.stdout = stream1 # capture stdout to a stream
-                            match0.print_board() # not implemented yet
-
-                            sys.stdout = stdout_ # restore the previous stdout.
-                            print_side_by_side(stream0.getvalue(), stream1.getvalue())
-
-                            stream0.close()
-                            stream1.close()
-
-
-                    # Handle end of game details
-                    stream0 = io.StringIO()
-                    sys.stdout = stream0 # capture stdout to a stream
-                    match0.end_of_game()
-
-                    stream1 = io.StringIO()
-                    sys.stdout = stream1 # capture stdout to a stream
-                    match1.end_of_game()
-
-                    sys.stdout = stdout_ # restore the previous stdout.
-                    print_side_by_side(stream0.getvalue(), stream1.getvalue())
-
-                    stream0.close()
-                    stream1.close()
-
-                except:
-                    raise
-                finally:
-                    # Kill all subprocesses even if a crash
-                    for p in match0.player_processes:
-                        if p: p.kill()
-                    for p in match1.player_processes:
-                        if p: p.kill()
-
+            else:  # double_game (show side by side)
+                self.play_double_game()
 
         else:
-            program_names = self.player_list
-            t = Tournament(self.number_of_games, self.player_list, self.game_arity, self.time_limits, self.verbose, self.show_map)
-            #cProfile.runctx('t.play_all_games()', None, locals())
-            t.play_all_games()
-            t.print_win_data()
+            self.play_tournament()
+
+    def play_single_game(self):
+        try:
+            # Initialization
+            match = Match(0, self.config_str, self.player_list, self.time_limits, self.verbose, self.show_map)
+
+            match.pregame()
+            # Game Loop
+            while match.is_active():
+                # Play one round of the game
+                match.one_turn()
+
+            # Handle end of game details
+            match.end_of_game()
+
+        except:
+            raise
+        finally:
+            # Kill all subprocesses even if a crash
+            for p in match.player_processes:
+                if p:
+                    p.kill()
+
+    def play_double_game(self):
+        player_names = list(set(self.player_list))
+        reverse_player_list = []
+        for p in self.player_list:
+            reverse_player_list.append(player_names[1] if p == player_names[0]
+                                       else player_names[0])
+        try:
+            # Initialization (don't pass show map)
+            match0 = Match(0, self.config_str, self.player_list, self.time_limits, self.verbose, False)
+            match1 = Match(1, self.config_str, reverse_player_list, self.time_limits, self.verbose, False)
+
+            stdout_ = sys.stdout  # Keep track of the previous value.
+
+            stream0 = io.StringIO()
+            sys.stdout = stream0  # capture stdout to a stream
+            match0.pregame()
+
+            stream1 = io.StringIO()
+            sys.stdout = stream1  # capture stdout to a stream
+            match1.pregame()
+
+            sys.stdout = stdout_  # restore the previous stdout.
+            print_side_by_side(stream0.getvalue(), stream1.getvalue())
+
+            stream0.close()
+            stream1.close()
+
+            # Game Loop
+            while match0.is_active() or match1.is_active():
+                # Play one round of each game
+                stream0 = io.StringIO()
+                sys.stdout = stream0  # capture stdout to a stream
+                if match0.is_active():
+                    match0.one_turn()
+
+                stream1 = io.StringIO()
+                sys.stdout = stream1  # capture stdout to a stream
+                if match1.is_active():
+                    match1.one_turn()
+
+                sys.stdout = stdout_  # restore the previous stdout.
+                print_side_by_side(stream0.getvalue(), stream1.getvalue())
+
+                stream0.close()
+                stream1.close()
+
+                if self.show_map:
+                    stream0 = io.StringIO()
+                    sys.stdout = stream0  # capture stdout to a stream
+                    match0.print_board()  # not implemented yet
+
+                    stream1 = io.StringIO()
+                    sys.stdout = stream1  # capture stdout to a stream
+                    match0.print_board()  # not implemented yet
+
+                    sys.stdout = stdout_  # restore the previous stdout.
+                    print_side_by_side(stream0.getvalue(), stream1.getvalue())
+
+                    stream0.close()
+                    stream1.close()
 
 
+            # Handle end of game details
+            stream0 = io.StringIO()
+            sys.stdout = stream0  # capture stdout to a stream
+            match0.end_of_game()
 
+            stream1 = io.StringIO()
+            sys.stdout = stream1  # capture stdout to a stream
+            match1.end_of_game()
+
+            sys.stdout = stdout_  # restore the previous stdout.
+            print_side_by_side(stream0.getvalue(), stream1.getvalue())
+
+            stream0.close()
+            stream1.close()
+
+        except:
+            raise
+        finally:
+            # Kill all subprocesses even if a crash
+            for p in match0.player_processes:
+                if p:
+                    p.kill()
+            for p in match1.player_processes:
+                if p:
+                    p.kill()
+
+    def play_tournament(self):
+        t = Tournament(self.number_of_games, self.player_list, self.game_arity, self.time_limits, self.verbose, self.show_map)
+        t.play_all_games()
+        t.print_win_data()
 
 
 
